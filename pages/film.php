1663942a4e0066640cd2262fca5a5e91d70eb86b
Brukhamer <seriv400@gmail.com>
2023-10-03 08:07:04 +0400

    3 comit

--- pages/film.php
+++ pages/film.php
@@ -3,8 +3,9 @@ session_start();
 require '../php/db.php';
 
 $id = $_GET['id'];
-$film = select('select * from films where id =:id', ['id =>$id']);
+$film = select('select * from films where id =:id', ['id' =>$id]);
 ?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -18,11 +19,12 @@ $film = select('select * from films where id =:id', ['id =>$id']);
 <body>
 <?php require '../Modules/header.php'?>
 <?php foreach ($film as $row):?>
-    <img src="data:image/jpg;base64, <?=$row['cover']?>" alt="cover">
+    <img style="width: 250px; height: 250px;" src="data:image/png;base64,<?= $row['cover']?>" alt="">
     <h3><?=$row['desc']?></h3>
     <span><?=$row['grade']?>*</span>
     <span><?=$row['price']?>P</span>
-    <form action="../"></form>
+
+    <form action="../php/add_card.php" method="post"><input style="display: none" type="text" value="<?=$_GET['id'] ?>" name='id'><input type="submit" value="Купить"></form>
 <?php endforeach; ?>
 </body>
 </html>
